20432f3dc179f0a338ba74d79cc2307372eccdaa
Clement Siaffu <clement@shuledirect.org>
2021-07-05 09:20:42 +0300

    add then in mongoose

--- model/db.js
+++ model/db.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose")
 const uri = "mongodb+srv://Clement:Cle*1995@cluster0-my6sq.mongodb.net/Meter?retryWrites=true&w=majority"
+
 mongoose.connect(uri,{useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>console.log("mongodb connected"))
+.catch(err=>console.log("there is an error"))
+
 require('./stucture')
